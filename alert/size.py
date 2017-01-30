2704824f7310ac090d53be7af6030e17e70804fb
Frank Bertsch <fbertsch@mozilla.com>
2017-01-29 21:29:26 -0600

    Move script to separate file, update size.py
    Script is now a separate file with all arguments.
    
    Size.py is now ordered by top-down function usage, more readable
    in my opinion.
    
    Added some new options, including from email and to email, rather
    than having those be constant.

--- alert/size.py
+++ alert/size.py
@@ -6,28 +6,100 @@ import boto3
 
 from mail import send_ses
 
-TELEMETRY_ADDR = 'telemetry-alerts@mozilla.com'
-DEV_TELEMETRY_ADDR = 'dev-telemetry-alerts@mozilla.com'
 
 EMAIL_SUBJECT = 'ACTION NEEDED: EFS Exceeds Allowed Size'
 EMAIL_BODY = ('EFS file system {} has exceeded the allowed size of {}. '
               'It is currently at {}.')
+
+EMAIL_SUBJECT_USER_LIST = 'List of Users Exceeding EFS Quota'
 
 USER_EMAIL_SUBJECT = 'EFS Directory Too Large!'
 USER_EMAIL_BODY = ('Attention, your EFS directory has exceeded the allowed quota. '
               'You are allowed {}, but have stored {}. '
               'Please remove at least {}, or we will have to remove your account.')
 
+
+def run_checks(efs_name, max_efs_size, from_email, to_email,
+                directory=None, user_depth=1, user_max_size=1024*1024*1024,
+                email_users=False, dry_run=False):
+    '''Run checks on sizes for EFS file system. First checks the total size, if it exceeds
+       some threshold, then optionally checks each user's dir to see if it is exceeding a quota.
+
+       If directory is None, the user directories will not be checked for size limits.
+
+       param efs_name: The name of the EFS file system to check
+       param max_efs_size: The max allowed size of the file system
+       param from_email: Email address alert emails are sent from
+       param to_email: Email address alert emails are sent to
+       param directory: Local path to the directory where this EFS file system is mounted (default None)
+       param user_depth: Depth of user directories from mounted directory (default 1)
+       param user_max_size: Max size of a user directory (default 1 GB)
+       param email_user: Boolean, whether to email users about size violations (default False)
+       param dry_run: If True, email will not be sent, but printed to the screen instead
+    '''
+    efs_size = _get_efs_size(efs_name)
+
+    if efs_size > max_efs_size:
+        _email_size_too_large(from_email, to_email, efs_name, efs_size, max_efs_size, dry_run)
+
+    if directory:
+        _check_user_sizes(directory, user_depth, user_max_size, from_email, to_email, email_users, dry_run)
+
+
+###### Private Functions #######
+
+
+def _get_efs_size(efs_name):
+    '''Get the size of an EFS file system, by name'''
+    client = boto3.client('efs')
+    response = client.describe_file_systems()
+
+    file_systems = response['FileSystems']
+    file_system = filter(lambda x: x['Name'] == efs_name, file_systems)
+
+    #TODO: deal with non-unique fs names
+    return file_system[0]['SizeInBytes']['Value']
+
+
+def _email_size_too_large(from_email, to_email, efs_name, size, max_size, dry_run):
+    '''Emails that the EFS file system is too large'''
+    email_body = EMAIL_BODY.format(efs_name,
+                                   _get_readable_size(max_size),
+                                   _get_readable_size(size))
+
+    if dry_run:
+        _print_email(to_email, from_email, EMAIL_SUBJECT, email_body)
+    else:
+        send_ses(from_email, USER_EMAIL_SUBJECT, email_body, to_email)
+
+
+def _check_user_sizes(directory, depth, max_size, from_email, to_email, email_users, dry_run):
+    '''Checks the sizes of user dirs. Directory must be mounted locally.
+       If size of a user dir exceeds max_size, send an email'''
+    invalid_users = []
+
+    for check_dir in _get_dirs(directory, depth):
+        size = _get_dir_size(check_dir)
+        if size > max_size:
+            invalid_users.append((check_dir, size, max_size))
+
+    if invalid_users:
+        _email_about_users(invalid_users, max_size, from_email, to_email, dry_run)
+        if email_users:
+            _email_users(invalid_users, max_size, from_email, to_email, dry_run)
+
+
 def _get_dirs(directory, depth):
     '''Get list of directories at depth from given directory'''
-    directories = []
+    directories = set()
     directory_depth = directory.rstrip(os.path.sep).count(os.path.sep)
     for path, dirs, files in os.walk(directory):
-        cur_depth = path.count(os.path.sep)
+        cur_depth = path.rstrip(os.path.sep).count(os.path.sep)
         if (cur_depth + 1) - directory_depth == depth:
             abs_path = os.path.abspath(path)
-            directories += [os.path.join(abs_path, d) for d in dirs]
-    return directories 
+            directories |= set([os.path.join(abs_path, d) for d in dirs])
+    return directories
+
 
 def _get_dir_size(directory):
     '''Get directory size in Bytes'''
@@ -38,6 +110,35 @@ def _get_dir_size(directory):
             total_size += os.path.getsize(fp)
     return total_size
 
+
+def _email_about_users(invalid_users, max_size, from_email, to_email, dry_run):
+    email_body = '\n'.join(('{}: {}, Quota: {}'.format(directory, 
+                                                       _get_readable_size(size), 
+                                                       _get_readable_size(max_size))
+                            for directory, size, max_size in invalid_users))
+    if dry_run:
+        _print_email(to_email, from_email, EMAIL_SUBJECT_USER_LIST, email_body)
+    else:
+        send_ses(from_email, EMAIL_SUBJECT_USER_LIST, email_body, to_email)
+
+
+def _email_users(invalid_users, max_size, from_email, to_email, dry_run):
+    '''Emails users about the size of their directories.
+       This assumes directory names are the user's email'''
+    for directory, size, max_size in invalid_users:
+        _, email = os.path.split(directory)
+        email_body = USER_EMAIL_BODY.format(_get_readable_size(max_size),
+                                            _get_readable_size(size),
+                                            _get_readable_size(size - max_size))
+        if dry_run:
+            _print_email(email, from_email, USER_EMAIL_SUBJECT, email_body)
+        else:
+            send_ses(from_email, USER_EMAIL_SUBJECT, email_body, email)
+
+
+##### Utility Functions #######
+
+
 def _get_readable_size(size_in_bytes, rounding=1):
     '''Max out at 1024YB (future proof! Get at me, EFS!)'''
     for unit in ['','K','M','G','T','P','E','Z','Y']:
@@ -46,91 +147,9 @@ def _get_readable_size(size_in_bytes, rounding=1):
         size_in_bytes /= 1024
     return None
 
-def _email_users(to_email, dry_run):
-    '''Emails users about the size of their directories.
-       This assumes directory names are the user's email'''
-    for directory, size, max_size in to_email:
-        _, email = os.path.split(directory)
-        email_body = USER_EMAIL_BODY.format(_get_readable_size(max_size),
-                                            _get_readable_size(size),
-                                            _get_readable_size(size - max_size))
-        if dry_run:
-            print ('\n\nSending Email to {}\n'
-                  'From: {}\n'
-                  'Subject: {}\n'
-                  'Body: {}\n').format(email, TELEMETRY_ADDR, USER_EMAIL_SUBJECT, email_body)
-        else:
-            send_ses(TELEMETRY_ADDR, USER_EMAIL_SUBJECT, email_body, email)
 
-def _check_sizes(directory, depth, max_size, dry_run):
-    '''Checks the sizes of user dirs. Directory must be mounted locally.
-       If size of a user dir exceeds max_size, send an email''' 
-    to_email = []
-
-    for check_dir in _get_dirs(directory, depth):
-        size = _get_dir_size(check_dir)
-        if size > max_size:
-            to_email.append((check_dir, size, max_size))
-
-    _email_users(to_email, dry_run)
-
-def _get_efs_size(efs_name):
-    '''Get the size of an EFS file system, by name'''
-    client = boto3.client('efs')
-    response = client.describe_file_systems()
-
-    file_systems = response['FileSystems']
-    file_system = filter(lambda x: x['Name'] == efs_name, file_systems)
-
-    return file_system[0]['SizeInBytes']['Value']
-
-def _email_size_too_large(efs_name, size, max_size, dry_run):
-    '''Emails telemetry-dev know that the EFS file system is too large'''
-    email_body = EMAIL_BODY.format(efs_name, 
-                                   _get_readable_size(max_size),
-                                   _get_readable_size(size))
-    if dry_run:
-        print ('\n\nSending Email to {}\n'
-              'From: {}\n'
-              'Subject: {}\n'
-              'Body: {}\n').format(DEV_TELEMETRY_ADDR, TELEMETRY_ADDR, USER_EMAIL_SUBJECT, email_body)
-    else:
-        send_ses(TELEMETRY_ADDR, USER_EMAIL_SUBJECT, email_body, DEV_TELEMETRY_ADDR)
-
-
-def run_checks(efs_name, max_efs_size, directory, user_depth, user_max_size, dry_run):
-    '''Run checks on sizes for EFS file system. First checks the total size, if it exceeds
-       some threshold, checks each user's dir to see if it is exceeding our quota.'''
-    efs_size = _get_efs_size(efs_name)
-
-    if efs_size > max_efs_size:
-        _email_size_too_large(efs_name, efs_size, max_efs_size, dry_run)
-
-        if directory:
-            _check_user_sizes(directory, user_depth, user_max_size, dry_run)
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='EFS Metered Size Alert System',
-                                    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-
-    parser.add_argument('--efs-name', type=str, required=True,
-                        help='Name of EFS file system to check')
-
-    parser.add_argument('--max-size', type=int, required=True,
-                        help='Max allowed size of file system')
-
-    parser.add_argument('--efs-dir', type=str, default=None,
-                        help='Local directory in which to check sizes (should be where EFS is mounted)')
-
-    parser.add_argument('--max-user-size', type=int, default=1024*1024*1024,
-                        help='Maximum allowed size of a directory, in Bytes')
-
-    parser.add_argument('---user-dir-depth', '-d', type=int, default=1,
-                        help='Directories at this depth from `EFS-Directory` will be checked')
-
-    parser.add_argument('--dry-run', action='store_true', default=False,
-                        help='If True, emails to users will not be sent, but results printed to screen')
-
-    args = parser.parse_args()
-
-    run_checks(args.efs_name, args.max_size, args.efs_dir, args.user_dir_depth, args.max_user_size, args.dry_run)
+def _print_email(to, _from, subject, body):
+    print ('\n\nSending Email to {}\n'
+           'From: {}\n'
+           'Subject: {}\n'
+           'Body: {}\n').format(to, _from, subject, body)
